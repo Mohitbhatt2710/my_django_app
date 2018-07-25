6ed7149c7927cb55ed5d5f7c0a15195f46ef047b
Mohit Bhatt <mohitbhatt2710@gmail.com>
2018-07-25 21:11:16 +0530

    DjangoWhiteNoise

--- zomato/wsgi.py
+++ zomato/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhitenoise
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zomato.settings")
 
